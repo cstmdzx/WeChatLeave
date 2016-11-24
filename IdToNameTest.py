fcea95469b3c1a68b80a824bae91981ebca34931
= <=>
2016-11-24 15:02:01 +0800

    solve insert name luanma

---
+++ IdToNameTest.py
@@ -0,0 +1,5 @@
+import MySQLdb
+
+db = MySQLdb.connect('localhost', 'root', 'dbis23508468', 'WeChatLeave')
+
+
--- InsertName.py
+++ InsertName.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
+import sys
+import MySQLdb
+
 reload(sys)  # 用来使后面的输出是中文
 sys.setdefaultencoding('utf-8')
-fileName = open('WeChatIdToName.txt', 'r')
+fileName = open('id2', 'r')
 linesName = fileName.readlines()
 db = MySQLdb.connect('localhost', 'root', 'dbis23508468', 'WeChatLeave')
 cursor = db.cursor()
+
+# to ensure that the characters are Chinese after insertion
+db.set_character_set('utf8')
+cursor.execute('SET NAMES utf8')
+cursor.execute('SET CHARACTER SET utf8')
+cursor.execute('SET character_set_connection=utf8')
+
 for eachName in linesName:
+    eachName = eachName.replace('^M', '')
+
+
+
     eachName = eachName.replace('\n', '')
+    print eachName
     eachName = eachName.replace('\r', '')
     words = eachName.split(' ')
 
-    cursor.execute('insert into WeChatIdToName values(\'' + words[0].__str__() + '\', \'' + words[1].__str__() + '\')')
-db.commit()
+    print eachName
+    print words[1]
+    sql = 'update WeChatIdToName set StudentName = \'' + words[1].__str__() + '\' where WeChatId = \'' + words[0].__str__() + '\''
+    sql2 = 'update WeChatIdToName set StudentName = \'yu用来使后\' where WeChatId = \'yu327018593\''
+    cursor.execute(sql)
+    print sql
+    db.commit()
 db.close()
 
